e3d63fb9d035cc668a78200d63d219b48b40676e
C4mperpr0 <Carlbellgardt@gmail.com>
2022-03-30 22:42:08 +0200

    sync

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from pssh.clients import SSHClient
 
 hosts = []
 while 1:
-    new_ip = input()
+    new_ip = input("IP (ENTER for done):")
     if new_ip == '':
         break
     hosts.append(new_ip)
@@ -10,7 +10,7 @@ while 1:
 client = ParallelSSHClient(hosts)
 
 while 1:
-    output = client.run_command(input())
+    output = client.run_command(input("Command:"))
     for host_output in output:
         for line in host_output.stdout:
             print(line)
