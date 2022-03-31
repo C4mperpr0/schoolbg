e40292bfc167e1dc4b73081052c00717f404eaf5
c4mperpr0 <carlbellgardt@gmail.com>
2022-03-31 16:04:39 +0200

    sync

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 from pssh.clients import ParallelSSHClient
+import ipscan
 
+ipscan.ipscan("192.168.103.xxx")
+
+"""
 hosts = []
 while 1:
     new_ip = input("IP (ENTER for done):")
     if new_ip == '':
         break
     hosts.append(new_ip)
+
 
 client = ParallelSSHClient(hosts)
 
@@ -14,5 +19,5 @@ while 1:
     for host_output in output:
         for line in host_output.stdout:
             print(line)
-
+"""
 
